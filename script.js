605f5199da5bc8d3a87a8b4cfb754dbfdaa30943
axxmv <andreavcruzm@gmail.com>
2025-07-13 19:44:10 -0500

    gelocation feature

--- script.js
+++ script.js
@@ -30,7 +30,6 @@ button.addEventListener("click", function()
         const icon = data.current.condition.icon;
 
         const uom = document.getElementById("uom");
-        const units = uom.value;
 
 
         if(uom.checked)
@@ -58,3 +57,70 @@ button.addEventListener("click", function()
     });
 });
 
+
+const current_location = document.getElementById("current_location");
+current_location.addEventListener("click", function()
+{
+    console.log("working");
+    alert("working");
+
+    //navigator is an object in all browsers
+    //geolocation is a property of navigator
+    //getCurrentPosition is the function
+    navigator.geolocation.getCurrentPosition(success, error); //success is a function( a callback function)
+
+});
+
+function success(position)
+{
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+
+    lonlatweather(lat, lon);
+
+};
+function error(err) //required in order for the geolocation to work
+{
+    displayWeather.innerHTML = `<p>Geolocation error: ${err.message}</p>`;
+    console.error("Geolocation error:", err);
+}
+function lonlatweather(lat, lon)
+{
+    const url = `${baseUrl}/current.json?key=${apikey}&q=${lat},${lon}`
+    console.log("Fetching weather from:", url);
+
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>{
+
+        const temperatureF = data.current.temp_f;//you looked at the json format to come up with this btw
+        const temperatureC = data.current.temp_c;
+        const condition = data.current.condition.text;
+        const location = `${data.location.name}, ${data.location.region}`;
+        const icon = data.current.condition.icon;
+
+        const uom = document.getElementById("uom");
+
+        if(uom.checked)
+        {
+        displayWeather.innerHTML = `
+        <h3>Weather for ${location}: </h3>
+        <p>Temperature: ${temperatureC}</p>
+        <p>Conditions: ${condition}</p>
+        <img src = "https:${icon}" alt = ":(">
+        `
+        }
+        else
+        {
+            displayWeather.innerHTML = `
+            <h3>Weather for ${location}: </h3>
+            <p>Temperature: ${temperatureF}</p>
+            <p>Conditions: ${condition}</p>
+            <img src = "https:${icon}" alt = ":(">
+            `
+
+        }
+    });
+
+}
