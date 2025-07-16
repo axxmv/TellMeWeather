5a06df159f9824a94cf9c9a158fc4de2739eedb7
axxmv <andreavcruzm@gmail.com>
2025-07-16 09:06:44 -0500

    5 day forcast

--- script.js
+++ script.js
@@ -14,7 +14,8 @@ button.addEventListener("click", function()
     let state = stateInput.value;
 
     let query = state ? `${city},${state}` : city;
-    const url = `${baseUrl}/current.json?key=${apikey}&q=${encodeURIComponent(query)}`;
+    //const url = `${baseUrl}/current.json?key=${apikey}&q=${encodeURIComponent(query)}`;
+    const url = `${baseUrl}/forecast.json?key=${apikey}&q=${encodeURIComponent(query)}&days=5`;
 
     displayWeather.innerHTML = "Getting Weather Information";
 
@@ -51,7 +52,7 @@ button.addEventListener("click", function()
             `
 
         }
-
+        display5day(data, uom.checked);
 
 
     });
@@ -61,8 +62,6 @@ button.addEventListener("click", function()
 const current_location = document.getElementById("current_location");
 current_location.addEventListener("click", function()
 {
-    console.log("working");
-    alert("working");
 
     //navigator is an object in all browsers
     //geolocation is a property of navigator
@@ -86,7 +85,8 @@ function error(err) //required in order for the geolocation to work
 }
 function lonlatweather(lat, lon)
 {
-    const url = `${baseUrl}/current.json?key=${apikey}&q=${lat},${lon}`
+    //const url = `${baseUrl}/current.json?key=${apikey}&q=${lat},${lon}`
+    const url = `${baseUrl}/forecast.json?key=${apikey}&q=${lat},${lon}&days=5`;
     console.log("Fetching weather from:", url);
 
 
@@ -119,8 +119,47 @@ function lonlatweather(lat, lon)
             <p>Conditions: ${condition}</p>
             <img src = "https:${icon}" alt = ":(">
             `
-
         }
+        display5day(data, uom.checked);
     });
+
+}
+
+function display5day(data, useCelsius)
+{
+    //forecastday is an array property provided by the api
+
+    //now days is an array we defined using the info from the api
+    let days = data.forecast.forecastday;
+    let unit = useCelsius ? "°C" : "°F";
+    let forecastHTML = "";
+
+    days.forEach(function(day)//forEach expects a function so it can call it for each item
+    {
+        let rawDate = new Date(day.date);// Convert the date string from API into a JavaScript Date object
+
+        let weekday = rawDate.toLocaleDateString(undefined, {weekday:'short'});
+        let month = rawDate.toLocaleDateString(undefined, { month: 'short' });
+        let dateNumber = rawDate.getDate(); // just the number of the day (e.g. 14)
+
+        let icon = day.day.condition.icon;
+        let text = day.day.condition.text;
+        let max = useCelsius ? day.day.maxtemp_c : day.day.maxtemp_f;
+        let min = useCelsius ? day.day.mintemp_c : day.day.mintemp_f;
+
+        forecastHTML +=
+        `
+        <p><strong>${weekday}, ${month} ${dateNumber}</strong></p>
+        <img src = "https:${icon}">
+        <p>${text}</p>
+        <p>↑ ${max}${unit}</p>
+        <p>↓ ${min}${unit}</p>
+        `;
+
+        
+    });
+
+    let forecastWeather = document.getElementById("forecastWeather");
+    forecastWeather.innerHTML = forecastHTML;
 
 }
